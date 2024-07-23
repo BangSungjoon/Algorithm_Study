7bea0837cea00685cc8381070c2b5d5d02f0cac0
hyunwoo <aduma489@gmail.com>
2024-07-23 14:58:52 +0900

    [SEA] 2072 / d1 / 10m / 최현우

---
+++ chw/week02/2072/solve.py
@@ -0,0 +1,41 @@
+import sys
+
+sys.stdin = open('input.txt')
+
+T = int(input())
+# number_list = [list(map(int, input().split())) for t in range(T)]
+
+for t in range(T):
+    sum_odd = 0
+    row = list(map(int, input().split()))
+    for num in row:
+        if num % 2 == 1:
+            sum_odd = sum_odd + num
+    print(f'#{t+1} {sum_odd}')
+
+
+
+
+
+
+
+
+
+
+
+
+# line1 = int_num[1:11]
+# line2 = int_num[11:21]
+# line3 = int_num[21:]
+
+# def add_odd(numbers, *args):
+#     result = 0
+#     for i in args:
+#         if i%0 != 0:
+#             result = result + i
+#         else:
+#             pass
+#         return result
+
+# print(add_odd(line1))
+
