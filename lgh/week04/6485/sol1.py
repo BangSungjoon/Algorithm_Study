fd701245a1a257f4f2bbab0039bbab32336c846d
Taeran <ktr504@naver.com>
2024-08-08 18:31:58 +0900

    가희 미완

---
+++ lgh/week04/6485/sol1.py
@@ -0,0 +1,19 @@
+import sys
+sys.stdin=open('s_input (1).txt')
+
+T=int(input())
+for tc in range(1,T+1):
+    N=int(input())
+    arr= [list(input().split())for _ in range(N)]
+    # print(arr)    
+    P=int(input())
+    C=[int(input())for _ in range(N)] #막힘
+    res=[0]*P
+    # # print(C)
+   
+    for i in range(N):  #막힘
+        for j in range(P):
+            if c
+            
+  
+    # print(C)   
