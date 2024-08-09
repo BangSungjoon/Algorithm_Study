e4ac1039e831da60290c41ed92ef89981be0fee6
nansae0804 <nansae0804@gmail.com>
2024-08-09 09:36:34 +0900

    [SEA] 1945 / D2 / 15M / 조현희

---
+++ jhh/week04/1945/1945_sol1.py
@@ -0,0 +1,22 @@
+import sys
+sys.stdin = open('input.txt')
+
+T = int(input())
+for tc in range(1, T+1):
+    N = int(input())
+ 
+    def seperate(N, a):
+        count_num = 0
+        while N % a == 0:
+            count_num += 1
+            N = N // a
+        return count_num
+ 
+    under_lst = [2, 3, 5, 7, 11]
+    print(f'#{tc}', end=' ')
+    for num in under_lst:
+        print(seperate(N, num), end=' ')
+    print()
+
+
+
