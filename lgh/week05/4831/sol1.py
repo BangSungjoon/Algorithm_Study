1fcd306e816a81a66904cb12060623f7483e94f1
Taeran <ktr504@naver.com>
2024-08-14 13:50:20 +0900

    [SEA] 4831/ d2 / 이가희

---
+++ lgh/week05/4831/sol1.py
@@ -0,0 +1,23 @@
+import sys 
+sys.stdin=open('sample_input (1).txt')
+
+
+T= int(input())
+for tc in range(1,T+1):
+    K, N, M =map(int, input().split())
+    M_arr=list(map(int, input().split()))
+    # print(K, N, M)
+    # print(M_arr)
+    start=0
+    res=0
+
+    for i in range(1,M+2): #도착지 N포함이므로 도착지N의 인덱스(M+1)까지
+        if M_arr[i] - M_arr[i-1] > K:
+            res = 0
+            break
+        if M_arr[i] - start > K:
+            start = M_arr[i-1]
+            res += 1
+
+    print(f'#{tc}', res)        
+
