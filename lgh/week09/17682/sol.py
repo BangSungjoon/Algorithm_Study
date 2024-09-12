509af54c1181b289c6e095429e9afaedb445d888
2gahee <lgh7956@naver.com>
2024-09-12 17:07:58 +0900

    [PGM] 17682  / levle 2 / 1h 30 / 이가희

--- lgh/week09/17682/sol.py
+++ lgh/week09/17682/sol.py
@@ -1,25 +1,39 @@
-def solution(dartResult):
-    n = ''   #숫자만을 담을 문자열
-    score = []  #계산된 값을 담을 리스트
-    for i in dartResult: 
-        if i.isdigit(): # 숫자면 n에 담아줌 
-            n += i
-        elif i == 'S':   # 'S'면   1제곱해주고 계산된값을 score리스트에 담음, 숫자문자열 n을 비워줌
-            score.append(int(n) ** 1)
-            n = ''
-        elif i == 'D':
-            score.append(int(n) ** 2)
-            n = ''
-        elif i == 'T':
-            score.append(int(n) ** 3)
-            n = ''
-        elif i == '#':
-            score[-1] = score[-1] * (-1)
-        elif i == '*':
-            if len(score) > 1:   # score 길이가 1개보다 많으면 뒤에서 2개 꺼내오고 1개면 뒤에서 1개만 꺼내옴 
-                score[-2] = score[-2] * 2
-                score[-1] = score[-1] * 2
-            else:
-                score[-1] = score[-1] *2
-    answer = sum(score) #score리스트에 담긴 값 더해줌
-    return answer
+def solution(str1, str2):
+    new_str1 =[]
+    new_str2 =[]
+    # str1을 입력받아와서 알파벳이면 2개씩 슬라이싱해서 new_str1에 넣어줌 
+    for i in range(len(str1)-1):
+        if str1[i:i+2].isalpha():
+            new_str1.append(str1[i:i+2])
+    # str2을 입력받아와서 알파벳이면 2개씩 슬라이싱해서 new_str2에 넣어줌 
+    for i in range(len(str2)-1):
+        if str2[i:i+2].isalpha():
+           new_str2.append(str2[i:i+2]) 
+    # new_str1을 소문자로 바꿔줌
+    for i in range(len(new_str1)):
+        new_str1[i] = new_str1[i].lower()
+    
+    # new_str2를 소문자로 바꿔줌
+    for i in range(len(new_str2)):
+        new_str2[i] = new_str2[i].lower()
+ 
+    #new_str2를 지워나가면서 교집합의 개수를 셀 것이므로, 미리 복사해둠 
+    copy_new_str2 = new_str2.copy()
+    intersection = 0  #교집합 개수
+
+    # new_str1에 담긴 문자열이 new_str2에 있다면 하나씩 지울때마다 교집합 개수 +1
+    for str in new_str1:
+        if str in new_str2:
+            copy_new_str2.remove(str)
+            intersection += 1
+    #합집합 
+    union = len(new_str1) + len(new_str2) - intersection
+    
+    #0나누기0은 1로 처리되므로 ans지정
+    if intersection == 0 and union==0:
+        ans = 65536 
+    else:
+        ans=int((intersection / union) * 65536 ) 
+    return ans
+
+    #sdf
