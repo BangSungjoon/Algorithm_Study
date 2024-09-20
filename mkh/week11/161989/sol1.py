f39661156d490365ee952e5148cf2cadba760906
계ì2gahee <lgh7956@naver.com>
2024-09-20 14:48:42 +0900

    [PGM] 5215 / lv1 / 20m / 민경현

--- mkh/week11/161989/sol1.py
+++ mkh/week11/161989/sol1.py
@@ -1,16 +1,10 @@
 def solution(n, m, section):
-    dists = [section[i+1]-section[i] for i in range(len(section)-1)]
     answer = 1
-    temp = 0
-    while dists:
-        if temp + dists[-1] > m-1:
+    current_end = section[0] + m - 1
+    
+    for s in section[1:]:
+        if s > current_end:
             answer += 1
-            temp = dists.pop()
-        else:
-            temp += dists.pop()
-
+            current_end = s + m - 1
+    
     return answer
-
-
-
-print(solution(8, 4, [2,3,6]))
