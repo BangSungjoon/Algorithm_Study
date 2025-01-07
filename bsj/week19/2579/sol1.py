230cd50d4e91e2cd7b91cee13c0687930e3ac279
Sungjoon <happypigs7.dev@gmail.com>
2025-01-07 17:04:17 +0900

    [BOJ] 2579 / S3 / 1H / 방ë방CCì ë방성준

--- bsj/week19/2579/sol1.py
+++ bsj/week19/2579/sol1.py
@@ -6,10 +6,11 @@ def dfs(idx, score, before):
     global n, result
     # 백트래킹
     if not before:  # 전에 두칸 올라왔다면 -> 한칸, 두칸 올라가기 둘다 가능
-        if score < back_floor[idx]:
+                    # but, 이 조건이 없으면 오답이 나옴, 그런데 왜 오답인지 모르겠음
+        if score < back_floor[idx]:     # 이전에 올랐던 기록보다 낮다면 가지치기
             return
         else:
-            back_floor[idx] = score
+            back_floor[idx] = score     # 이전 기록보다 높다면 점수 기록
 
     if idx == n-1 and score > result:     # 꼭대기 칸에 도달했고, 현재 점수가 최대 점수보다 높다면
         result = score
@@ -34,7 +35,7 @@ result = 0          # 총 점수의 최댓값
 back_floor= [0]*n
 
 dfs(0, floor[0], False)
-if n != 1:
+if n != 1:          # n이 1인 경우가 있어서 조건 추가
     dfs(1, floor[1], False)
 
 print(result)
