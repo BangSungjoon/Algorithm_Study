87c2f48089bb0aad469d32c3a2f9901cef253553
Sungjoon <happypigs7.dev@gmail.com>
2025-01-07 17:00:35 +0900

    [BOJ] 2579 / S3 / 1H / 방방성ì준

---
+++ bsj/week19/2579/sol1.py
@@ -0,0 +1,40 @@
+# 백준 계단 오르기
+def dfs(idx, score, before):
+    # idx: 현재 계단 칸
+    # score: 현재 점수
+    # before: 전에 한칸 올랐니? 맞으면 True
+    global n, result
+    # 백트래킹
+    if not before:  # 전에 두칸 올라왔다면 -> 한칸, 두칸 올라가기 둘다 가능
+        if score < back_floor[idx]:
+            return
+        else:
+            back_floor[idx] = score
+
+    if idx == n-1 and score > result:     # 꼭대기 칸에 도달했고, 현재 점수가 최대 점수보다 높다면
+        result = score
+        return
+    if before:              # 전에 한칸만 올랐었다면 무조건 두칸 올라가슈
+        if idx+2 <= n-1:    # 범위 넘어가나 체크
+            dfs(idx+2, score+floor[idx+2], False)
+        else:               # 범위 넘었으면 나가리여
+            return
+    else:
+        if idx+1 <= n-1:    # 한 칸만 올라가기
+            dfs(idx+1, score+floor[idx+1], True)
+        if idx+2 <= n-1:    # 두 칸 오르기
+            dfs(idx+2, score + floor[idx+2], False)
+
+
+n = int(input())    # 계단의 개수
+floor = [0]*n       # 계단
+for i in range(n):
+    floor[i] = int(input())
+result = 0          # 총 점수의 최댓값
+back_floor= [0]*n
+
+dfs(0, floor[0], False)
+if n != 1:
+    dfs(1, floor[1], False)
+
+print(result)
