5c599e9009c551fe36184f5da5dc0ec33a6d960a
Sungjoon <happypigs7.dev@gmail.com>
2025-03-31 18:12:47 +0900

    [BOJ] 4659 / S5 / 20M / 방성준

--- bsj/week28/4659/sol1.py
+++ bsj/week28/4659/sol1.py
@@ -1,41 +1,37 @@
-# 비밀번호 발음하기
-password = input()
 moum = 'aeiou'
-result = 'acceptable.'
-while password != 'end':
+
+while True:
+    password = input()
+    if password == 'end':
+        break
+
+    has_moum = any(c in moum for c in password)
+    if not has_moum:
+        print(f'<{password}> is not acceptable.')
+        continue
+
+    is_acceptable = True
     moum_cnt = 0
     zaum_cnt = 0
-    break_point = False
-
-    for i in range(5):
-        if moum[i] in password:
-            break
-    else:
-        print(f'<{password}> is not acceptable.')
-        password = input()
-        continue
 
     for i in range(len(password)):
         if password[i] in moum:
-            if zaum_cnt > 0:
-                zaum_cnt = 0
             moum_cnt += 1
+            zaum_cnt = 0
         else:
-            if moum_cnt > 0:
-                moum_cnt = 0
             zaum_cnt += 1
-        if moum_cnt == 3 or zaum_cnt == 3:
-            print(f'<{password}> is not acceptable.')
-            password = input()
-            break_point = True
-            break
-    if break_point:
-        continue
-    if i < len(password)-1 and password[i] != 'e' and password[i] != 'o':
-        if password[i] == password[i+1]:
-            print(f'<{password}> is not acceptable.')
-            password = input()
-            continue
+            moum_cnt = 0
 
-    print(f'<{password}> is acceptable.')
-    password = input()
+        if moum_cnt == 3 or zaum_cnt == 3:
+            is_acceptable = False
+            break
+
+        if i < len(password) - 1:
+            if password[i] == password[i+1] and password[i] not in ['e', 'o']:
+                is_acceptable = False
+                break
+
+    if is_acceptable:
+        print(f'<{password}> is acceptable.')
+    else:
+        print(f'<{password}> is not acceptable.')
