538aa3fb07c07c6a7b7d1633b057cfca651b747d
SungJoon <happypigs7@gmail.com>
2025-09-04 14:27:06 +0900

    [BOJ] 11726 / S3 / 20M / 방성준

--- bsj/week47/1012/sol1.py
+++ bsj/week47/1012/sol1.py
@@ -1,13 +1,35 @@
-# [백준] 요세푸스 문제 0
+# 유기농 배추
 from collections import deque
 
-n, k = map(int, input().split())
-people = deque(list(range(1, n+1)))
+dx = [-1, 0, 1, 0]
+dy = [0, -1, 0, 1]
 
-answer = []
+t = int(input())
 
-while people:
-    people.rotate(-k+1)
-    answer.append(people.popleft())
+for test_case in range(t):
+    m, n, k = map(int, input().split())
+    arr = [[0]*m for _ in range(n)]
+    answer = 0
 
-print('<' + ', '.join(map(str, answer)) + '>')
+    for _ in range(k):
+        x, y = map(int, input().split())
+        arr[y][x] = 1
+
+    for i in range(n):
+        for j in range(m):
+            if arr[i][j] == 1:
+                dq = deque([(j, i)])
+                arr[i][j] = 0
+                while dq:
+                    x, y = dq.popleft()
+
+                    for k in range(4):
+                        nx = x + dx[k]
+                        ny = y + dy[k]
+
+                        if 0 <= nx < m and 0 <= ny < n and arr[ny][nx] == 1:
+                            arr[ny][nx] = 0
+                            dq.append((nx, ny))
+                answer += 1
+
+    print(answer)
